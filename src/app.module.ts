5005582d1602b2270ce3c9e912b517d8f92dd59e
Lourenço Viguini Werneck <lourencovw@gmail.com>
2025-02-19 03:28:00 +0000

    Add @nestjs/config dependency and configure it in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessageModule } from './message/message.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MessageModule],
+  imports: [ConfigModule.forRoot(), MessageModule],
   controllers: [AppController],
   providers: [AppService],
 })
